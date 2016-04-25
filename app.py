55bada8c2596621c93a9055bdb6bbf7a82f9f255
Nathan Coleman <nathandanielcoleman@gmail.com>
2016-04-24 21:40:50 -0400

    Add endpoint for viewing submit page

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import render_template
 
 from feedback.urls import HELLO_URL
 from feedback.urls import VIEW_SESSION_URL
+from feedback.urls import VIEW_SUBMIT_URL
 
 
 app = Flask(__name__)
@@ -13,9 +14,14 @@ def hello():
     return render_template("hello.html")
 
 
-@app.route(VIEW_SESSION_URL, methods=["GET"])
+@app.route(VIEW_SESSION_URL, methods=['GET'])
 def view_session(session_code):
     return render_template("view_session.html", session_code=session_code)
+
+
+@app.route(VIEW_SUBMIT_URL, methods=['GET'])
+def view_submit(session_code):
+    return render_template("view_submit.html", session_code=session_code)
 
 
 if __name__ == "__main__":
--- feedback/urls.py
+++ feedback/urls.py
@@ -1,2 +1,3 @@
 HELLO_URL = '/'
 VIEW_SESSION_URL = '/session/<session_code>/'
+VIEW_SUBMIT_URL = '/session/<session_code>/submit/'
