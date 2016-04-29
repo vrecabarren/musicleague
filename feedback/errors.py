1ea1c8aaa09bbdbf6e78cb70026c8e2dbb234e24
Nathan Coleman <nathandanielcoleman@gmail.com>
2016-04-28 20:59:25 -0400

    Raise exception when Session with name exists

---
+++ feedback/errors.py
@@ -0,0 +1,3 @@
+class SessionExistsError(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
--- feedback/session.py
+++ feedback/session.py
@@ -1,7 +1,11 @@
+from feedback.errors import SessionExistsError
+
 from feedback.models import Session
 
 
 def create_session(name):
+    if get_session(name):
+        raise SessionExistsError('Session with name %s already exists' % name)
     new_session = Session(name=name)
     new_session.save()
     return new_session
