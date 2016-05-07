1cd8390d4c319539295b71cf5891aff774f58a09
Nathan Coleman <nathandanielcoleman@gmail.com>
2016-05-07 11:47:06 -0400

    Initial commit for Facebook login

--- feedback/models.py
+++ feedback/models.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 
+from flask.ext.security import RoleMixin
+from flask.ext.security import UserMixin
+
 from mongoengine import BooleanField
 from mongoengine import DateTimeField
 from mongoengine import Document
@@ -7,6 +10,7 @@ from mongoengine import EmbeddedDocument
 from mongoengine import EmbeddedDocumentListField
 from mongoengine import IntField
 from mongoengine import ListField
+from mongoengine import ReferenceField
 from mongoengine import StringField
 
 
@@ -31,3 +35,43 @@ class Session(Document):
         for submission in self.submissions:
             guids.update(submission.tracks)
             return url.format(title=self.name, guids=','.join(list(guids)))
+
+
+class Role(Document, RoleMixin):
+    name = StringField(max_length=80, unique=True)
+    description = StringField(max_length=255)
+
+
+class User(Document, UserMixin):
+    email = StringField(max_length=255)
+    password = StringField(max_length=255)
+    active = BooleanField(default=True)
+    confirmed_at = DateTimeField()
+    roles = ListField(ReferenceField(Role), default=[])
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+
+class Connection(Document):
+    id = IntField(primary_key=True)
+    user_id = ReferenceField(User)
+    provider_id = StringField()
+    provider_user_id = StringField()
+    access_token = StringField()
+    secret = StringField()
+    display_name = StringField()
+    profile_url = StringField()
+    image_url = StringField()
