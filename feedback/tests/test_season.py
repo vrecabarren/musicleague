ba66c6dd4dc29a86e982ae4fd5924b4d6417410f
Nathan Coleman <nathandanielcoleman@gmail.com>
2016-05-21 12:30:15 -0400

    Update unit test

--- feedback/tests/test_season.py
+++ feedback/tests/test_season.py
@@ -4,6 +4,7 @@ from unittest import TestCase
 
 from feedback.errors import SeasonExistsError
 from feedback.models import Season
+from feedback.models import User
 from feedback.season import create_season
 
 
@@ -13,4 +14,7 @@ class CreateSeasonTestCase(TestCase):
     def test_create_season_name_in_use(self, get_season):
         get_season.return_value = Season(name='name_in_use')
 
-        self.assertRaises(SeasonExistsError, create_season, 'name_in_use')
+        self.assertRaises(
+            SeasonExistsError,
+            create_season, 'name_in_use',
+            User(id='123', name='Test User', email='test_user@test_user.com'))
