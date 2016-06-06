8661bdc1f938cae0ee0e6281f4ccb9b0928f8662
Nathan Coleman <nathandanielcoleman@gmail.com>
2016-06-06 19:59:11 -0400

    Initialize due dates when creating new submission periods

--- feedback/submission_period.py
+++ feedback/submission_period.py
@@ -1,9 +1,16 @@
+from datetime import datetime
+from datetime import timedelta
+
 from feedback.models import SubmissionPeriod
 
 
 def create_submission_period(league):
     name = '%s - SP %s' % (league.name, len(league.submission_periods) + 1)
-    new_submission_period = SubmissionPeriod(name=name, league=league)
+    submission_due_date = datetime.now() + timedelta(days=5)
+    vote_due_date = submission_due_date + timedelta(days=2)
+    new_submission_period = SubmissionPeriod(
+        name=name, league=league, submission_due_date=submission_due_date,
+        vote_due_date=vote_due_date)
     new_submission_period.save()
 
     # Mark all other previous submission periods as not current
