bb082e5a28925a1def60de734c1b7ab6d7f8698e
Nathan Coleman <nathandanielcoleman@gmail.com>
2016-06-09 19:20:30 -0400

    Remove logging format string

--- feedback/__init__.py
+++ feedback/__init__.py
@@ -26,16 +26,13 @@ from settings import MONGO_DB_NAME
 app = Flask(__name__)
 app.secret_key = get_secret_key()
 
-logFormatStr = '[%(asctime)s] %(levelname)s: %(message)s'
-
 if is_deployed():
     host, port, username, password, db = parse_mongolab_uri()
     db = connect(db, host=host, port=port, username=username, password=password)
-    logging.basicConfig(format=logFormatStr,
-                        level=logging.DEBUG if is_debug() else logging.WARNING)
+    logging.basicConfig(level=logging.DEBUG if is_debug() else logging.WARNING)
 else:
     db = connect(MONGO_DB_NAME)
-    logging.basicConfig(format=logFormatStr, level=logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG)
 
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 
