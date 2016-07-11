64cdbe9613207a93369fe63db4cc4c111ceae73b
Nathan Coleman <nathandanielcoleman@gmail.com>
2016-07-10 22:56:17 -0400

    Stop hiding track inputs when showing spotify player

--- feedback/templates/modals/submit.js
+++ feedback/templates/modals/submit.js
@@ -3,12 +3,4 @@ $('.track-input').change(function() {
     var player = $($(this).siblings('iframe')[0]);
     var playerSrc = 'https://embed.spotify.com/?uri=' + input.val() + '&theme=white'
     player.attr('src', 'https://embed.spotify.com/?uri=' + input.val());
-    input.attr('type', 'hidden');
-    player.css('display', 'block');
-});
-
-$('#cancel').click(function() {
-    $('iframe').css('display', 'none');
-    $('.track-input').attr('type', 'text');
-    $('iframe').attr('src', '');
 });
