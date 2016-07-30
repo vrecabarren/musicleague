a1a84e14fdcf66c8b8cf7e00a4c820723199fc22
Nathan Coleman <nathandanielcoleman@gmail.com>
2016-07-30 12:25:36 -0500

    Send logged in users to profile, always

--- feedback/routes/__init__.py
+++ feedback/routes/__init__.py
@@ -1,6 +1,8 @@
 # flake8: noqa
 from flask import g
+from flask import redirect
 from flask import request
+from flask import url_for
 
 from feedback import app
 from feedback.models import League
@@ -33,6 +35,10 @@ HELLO_URL = '/'
 @app.route(HELLO_URL)
 @templated('hello.html')
 def hello():
+    # If user is logged in, always send to profile
+    if g.user:
+        return redirect(url_for('profile'))
+
     return {
         'user': g.user,
         'oauth_url': get_spotify_oauth().get_authorize_url(),
