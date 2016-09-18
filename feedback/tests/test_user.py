487013e1874831bc49c821579a3a3250134b43c6
Nathan Coleman <nathandanielcoleman@gmail.com>
2016-09-18 12:09:15 -0500

    Finish adding unit tests for user.py

--- feedback/tests/test_user.py
+++ feedback/tests/test_user.py
@@ -1,28 +1,42 @@
+from datetime import datetime
 from unittest import TestCase
-
-from mock import patch
 
 from feedback.errors import UserExistsError
 from feedback.models import User
+from feedback.user import create_or_update_user
 from feedback.user import create_user
 from feedback.user import get_user
+from feedback.user import get_user_by_email
 
 
 class CreateUserTestCase(TestCase):
 
-    @patch('feedback.user.get_user')
-    def test_create_user_existing_id(self, get_user_mock):
-        get_user_mock.return_value = User(id='1', email='test_user@test.com')
+    def setUp(self):
+        self.id = '123'
+        self.name = 'Test User'
+        self.email = 'test_user@test.com'
+        self.image_url = 'http://test.com/test_img.jpg'
+
+    def tearDown(self):
+        # Ensure user from previous test is removed
+        existing_user = get_user(self.id)
+        if existing_user:
+            existing_user.delete()
+
+    def test_create_user_existing_id(self):
+        User(id=self.id, name=self.name, email=self.email,
+             image_url=self.image_url, joined=datetime.utcnow()).save()
+
         self.assertRaises(
             UserExistsError,
-            create_user, '1', 'Test User', 'test_user@test.com', '')
+            create_user, self.id, self.name, self.email, self.image_url)
 
     def test_create_user_and_save(self):
-        created = create_user('123', 'Test User', 'test_user@test.com', '')
+        created = create_user(self.id, self.name, self.email, self.image_url)
         self.assertIsNotNone(created)
-        self.assertEqual('Test User', created.name)
-        self.assertEqual('test_user@test.com', created.email)
-        self.assertEqual('', created.image_url)
+        self.assertEqual(self.name, created.name)
+        self.assertEqual(self.email, created.email)
+        self.assertEqual(self.image_url, created.image_url)
 
         saved = get_user(created.id)
         self.assertIsNotNone(saved)
@@ -30,4 +44,108 @@ class CreateUserTestCase(TestCase):
         self.assertEqual(created.email, saved.email)
         self.assertEqual(created.image_url, saved.image_url)
         self.assertEqual(created.preferences, saved.preferences)
-        saved.delete()
+
+
+class CreateOrUpdateUserTestCase(TestCase):
+
+    def setUp(self):
+        self.id = '123'
+        self.name = 'Test User'
+        self.email = 'test_user@test.com'
+        self.image_url = 'http://test.com/test_img.jpg'
+
+    def tearDown(self):
+        # Ensure user from previous test is removed
+        existing_user = get_user(self.id)
+        if existing_user:
+            existing_user.delete()
+
+    def test_none_existing(self):
+        self.assertIsNone(get_user(self.id))
+
+        created = create_or_update_user(
+            self.id, self.name, self.email, self.image_url)
+
+        saved = get_user(self.id)
+        self.assertIsNotNone(saved)
+        self.assertEqual(created.name, saved.name)
+        self.assertEqual(created.email, saved.email)
+        self.assertEqual(created.image_url, saved.image_url)
+        self.assertEqual(created.preferences, saved.preferences)
+
+    def test_existing(self):
+        User(id=self.id, name=self.name, email=self.email,
+             image_url=self.image_url, joined=datetime.utcnow()).save()
+
+        updated = create_or_update_user(
+            self.id, 'New Name', 'new_email@test.com', self.image_url)
+
+        self.assertEqual('New Name', updated.name)
+        self.assertEqual('new_email@test.com', updated.email)
+
+        saved = get_user(self.id)
+        self.assertIsNotNone(saved)
+        self.assertEqual(updated.name, saved.name)
+        self.assertEqual(updated.email, saved.email)
+        self.assertEqual(updated.image_url, saved.image_url)
+        self.assertEqual(updated.preferences, saved.preferences)
+
+
+class GetUserTestCase(TestCase):
+
+    def setUp(self):
+        self.id = '123'
+        self.name = 'Test User'
+        self.email = 'test_user@test.com'
+        self.image_url = 'http://test.com/test_img.jpg'
+
+    def tearDown(self):
+        # Ensure user from previous test is removed
+        existing_user = get_user(self.id)
+        if existing_user:
+            existing_user.delete()
+
+    def test_none_existing(self):
+        self.assertRaises(User.DoesNotExist, User.objects.get, id=self.id)
+        self.assertIsNone(get_user(self.id))
+
+    def test_existing(self):
+        User(id=self.id, name=self.name, email=self.email,
+             image_url=self.image_url, joined=datetime.utcnow()).save()
+        self.assertIsNotNone(User.objects.get(id=self.id))
+
+        user = get_user(self.id)
+        self.assertIsNotNone(user)
+        self.assertEqual(self.name, user.name)
+        self.assertEqual(self.email, user.email)
+        self.assertEqual(self.image_url, user.image_url)
+
+
+class GetUserByEmailTestCase(TestCase):
+
+    def setUp(self):
+        self.id = '123'
+        self.name = 'Test User'
+        self.email = 'test_user@test.com'
+        self.image_url = 'http://test.com/test_img.jpg'
+
+    def tearDown(self):
+        # Ensure user from previous test is removed
+        existing_user = get_user_by_email(self.email)
+        if existing_user:
+            existing_user.delete()
+
+    def test_none_existing(self):
+        self.assertRaises(User.DoesNotExist, User.objects.get, id=self.id)
+        self.assertIsNone(get_user_by_email(self.email))
+
+    def test_existing(self):
+        User(id=self.id, name=self.name, email=self.email,
+             image_url=self.image_url, joined=datetime.utcnow()).save()
+        self.assertIsNotNone(User.objects.get(id=self.id))
+
+        user = get_user_by_email(self.email)
+        self.assertIsNotNone(user)
+        self.assertEqual(self.name, user.name)
+        self.assertEqual(self.email, user.email)
+        self.assertEqual(self.image_url, user.image_url)
