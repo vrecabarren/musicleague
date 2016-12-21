0f213a158b6609952f693eb66cb7a331a80d0218
Nathan Coleman <nathandanielcoleman@gmail.com>
2016-12-21 13:58:58 -0600

    Move Messenger logic into new file

---
+++ musicleague/messenger.py
@@ -0,0 +1,45 @@
+import httplib
+import json
+import requests
+
+from musicleague.environment import get_setting
+from musicleague.environment.variables import MESSENGER_PAGE_ACCESS_TOKEN
+
+
+def process_data(data):
+    if data["object"] == "page":
+
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
+
+                if messaging_event.get("message"):
+                    sender_id = messaging_event["sender"]["id"]
+                    message_text = messaging_event["message"]["text"]
+                    process_message(sender_id, message_text)
+
+                if messaging_event.get("delivery"):
+                    pass
+
+                if messaging_event.get("optin"):
+                    pass
+
+                if messaging_event.get("postback"):
+                    pass
+
+
+def process_message(sender_id, message_text):
+    send_message(sender_id, "got it, thanks!")
+
+
+def send_message(recipient_id, message_text):
+    access_token = get_setting(MESSENGER_PAGE_ACCESS_TOKEN)
+    params = {"access_token": access_token}
+    headers = {"Content-Type": "application/json"}
+    data = json.dumps({
+        "recipient": {"id": recipient_id},
+        "message": {"text": message_text}
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+                      params=params, headers=headers, data=data)
+    if r.status_code != httplib.OK:
+        pass
--- musicleague/routes/messenger.py
+++ musicleague/routes/messenger.py
@@ -1,13 +1,11 @@
 import httplib
-import json
-import requests
 
 from flask import request
 
 from musicleague import app
 from musicleague.environment import get_setting
-from musicleague.environment.variables import MESSENGER_PAGE_ACCESS_TOKEN
 from musicleague.environment.variables import MESSENGER_VERIFY_TOKEN
+from musicleague.messenger import process_data
 
 
 MESSENGER_HOOK_URL = '/messenger/'
@@ -35,40 +33,5 @@ def verify():
 @app.route(MESSENGER_HOOK_URL, methods=['POST'])
 def webhook():
     data = request.get_json()
-
-    if data["object"] == "page":
-
-        for entry in data["entry"]:
-            for messaging_event in entry["messaging"]:
-
-                if messaging_event.get("message"):
-                    sender_id = messaging_event["sender"]["id"]
-                    # recipient_id = messaging_event["recipient"]["id"]
-                    # message_text = messaging_event["message"]["text"]
-
-                    send_message(sender_id, "got it, thanks!")
-
-                if messaging_event.get("delivery"):
-                    pass
-
-                if messaging_event.get("optin"):
-                    pass
-
-                if messaging_event.get("postback"):
-                    pass
-
-    return "ok", httplib.OK
-
-
-def send_message(recipient_id, message_text):
-    access_token = get_setting(MESSENGER_PAGE_ACCESS_TOKEN)
-    params = {"access_token": access_token}
-    headers = {"Content-Type": "application/json"}
-    data = json.dumps({
-        "recipient": {"id": recipient_id},
-        "message": {"text": message_text}
-    })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
-                      params=params, headers=headers, data=data)
-    if r.status_code != httplib.OK:
-        pass
+    process_data(data)
+    return "OK", httplib.OK
