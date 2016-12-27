b780f2e052dd9dda0f902fa8d3744aa13feb81b9
Nathan Coleman <nathandanielcoleman@gmail.com>
2016-12-27 14:59:12 -0600

    Auto-dismiss system-generated alerts

--- musicleague/static/base.js
+++ musicleague/static/base.js
@@ -2,3 +2,11 @@ $('.linked').on("click", function() {
     var url = $(this).attr('data-href');
     window.location.href = url;
 });
+
+$(document).ready(function() {
+    setTimeout(function() {
+        $(".alert-auto-dismiss").fadeTo(500, 0).slideUp(500, function(){
+            $(this).remove();
+        });
+    }, 4000);
+});
