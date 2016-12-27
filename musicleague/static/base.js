d627bbbd4810e68b52d1b8b3878a1b8deb753405
Nathan Coleman <nathandanielcoleman@gmail.com>
2016-12-27 15:04:15 -0600

    Increase alert auto-dismiss to 10 seconds

--- musicleague/static/base.js
+++ musicleague/static/base.js
@@ -8,5 +8,5 @@ $(document).ready(function() {
         $(".alert-auto-dismiss").fadeTo(500, 0).slideUp(500, function(){
             $(this).remove();
         });
-    }, 4000);
+    }, 10000);
 });
