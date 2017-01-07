97e21c097d360deab44e1b46e49bb1337fa780da
Nathan Coleman <nathandanielcoleman@gmail.com>
2017-01-07 13:24:41 -0600

    Add unit tests for owner_user_submitted_notification

--- musicleague/tests/notify/test_notify.py
+++ musicleague/tests/notify/test_notify.py
@@ -4,6 +4,8 @@ from mock import patch
 
 from musicleague.league import create_league
 from musicleague.notify import owner_all_users_submitted_notification
+from musicleague.notify import owner_user_submitted_notification
+from musicleague.submission import create_submission
 from musicleague.submission_period import create_submission_period
 from musicleague.user import create_user
 
@@ -51,3 +53,51 @@ class OwnerAllUsersSubmittedNotificationTestCase(unittest.TestCase):
         self.assertTrue(sent)
         email.assert_called_once_with(self.user, self.submission_period)
         messenger.assert_called_once_with(self.user, self.submission_period)
+
+
+class OwnerUserSubmittedNotificationTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.owner = create_user('owner_id', 'Test Owner', 'test@user.com', '')
+        self.league = create_league(self.owner)
+        self.submission_period = create_submission_period(
+            self.league, 'Test League', 'Description')
+
+        self.user = create_user('id', 'Test User', 'test@user.com', '')
+        self.submission = create_submission(
+            [], self.submission_period, self.user, self.league)
+
+    def tearDown(self):
+        self.owner.delete()
+        self.league.delete()
+        self.submission_period.delete()
+        self.user.delete()
+        self.submission.delete()
+
+    def test_no_submission(self):
+        sent = owner_user_submitted_notification(None)
+        self.assertFalse(sent)
+
+    def test_no_owner(self):
+        self.league.owner = None
+        self.league.save()
+
+        sent = owner_user_submitted_notification(self.submission)
+        self.assertFalse(sent)
+
+    def test_disabled(self):
+        owner = self.league.owner
+        owner.preferences.owner_user_submitted_notifications = False
+        owner.save()
+
+        sent = owner_user_submitted_notification(self.submission)
+        self.assertFalse(sent)
+
+    @patch(PATCH_PATH + 'owner_user_submitted_email')
+    @patch(PATCH_PATH + 'owner_user_submitted_messenger')
+    def test_send(self, email, messenger):
+        sent = owner_user_submitted_notification(self.submission)
+
+        self.assertTrue(sent)
+        email.assert_called_once_with(self.owner, self.submission)
+        messenger.assert_called_once_with(self.owner, self.submission)
