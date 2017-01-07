e2351a3284b1a15a2e2cff543f917a42c34da243
Nathan Coleman <nathandanielcoleman@gmail.com>
2017-01-07 13:44:14 -0600

    Add unit tests for owner_user_voted_notification

--- musicleague/tests/notify/test_notify.py
+++ musicleague/tests/notify/test_notify.py
@@ -6,9 +6,11 @@ from musicleague.league import create_league
 from musicleague.notify import owner_all_users_submitted_notification
 from musicleague.notify import owner_all_users_voted_notification
 from musicleague.notify import owner_user_submitted_notification
+from musicleague.notify import owner_user_voted_notification
 from musicleague.submission import create_submission
 from musicleague.submission_period import create_submission_period
 from musicleague.user import create_user
+from musicleague.vote import create_vote
 
 
 PATCH_PATH = 'musicleague.notify.'
@@ -144,3 +146,51 @@ class OwnerAllUsersVotedNotificationTestCase(unittest.TestCase):
         self.assertTrue(sent)
         email.assert_called_once_with(self.user, self.submission_period)
         messenger.assert_called_once_with(self.user, self.submission_period)
+
+
+class OwnerUserVotedNotificationTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.owner = create_user('owner_id', 'Test Owner', 'test@user.com', '')
+        self.league = create_league(self.owner)
+        self.submission_period = create_submission_period(
+            self.league, 'Test League', 'Description')
+
+        self.user = create_user('id', 'Test User', 'test@user.com', '')
+        self.vote = create_vote(
+            {}, self.submission_period, self.user, self.league)
+
+    def tearDown(self):
+        self.owner.delete()
+        self.league.delete()
+        self.submission_period.delete()
+        self.user.delete()
+        self.vote.delete()
+
+    def test_no_submission(self):
+        sent = owner_user_voted_notification(None)
+        self.assertFalse(sent)
+
+    def test_no_owner(self):
+        self.league.owner = None
+        self.league.save()
+
+        sent = owner_user_voted_notification(self.vote)
+        self.assertFalse(sent)
+
+    def test_disabled(self):
+        owner = self.league.owner
+        owner.preferences.owner_user_voted_notifications = False
+        owner.save()
+
+        sent = owner_user_voted_notification(self.vote)
+        self.assertFalse(sent)
+
+    @patch(PATCH_PATH + 'owner_user_voted_email')
+    @patch(PATCH_PATH + 'owner_user_voted_messenger')
+    def test_send(self, email, messenger):
+        sent = owner_user_voted_notification(self.vote)
+
+        self.assertTrue(sent)
+        email.assert_called_once_with(self.owner, self.vote)
+        messenger.assert_called_once_with(self.owner, self.vote)
