6fcdef38d23a27191d664883fa9b2a93ee376131
Nathan Coleman <nathandanielcoleman@gmail.com>
2017-01-13 18:33:03 -0600

    Remove unused imports

--- scheduler.py
+++ scheduler.py
@@ -1,11 +1,5 @@
 
-from rq import Connection
-
-from musicleague import redis_conn
 from musicleague import scheduler
-
-
-listen = ['default']
 
 if __name__ == '__main__':
     scheduler.run(burst=False)
