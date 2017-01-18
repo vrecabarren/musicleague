b4b85a9a539797702ab3358db1df9e504e012335
Nathan Coleman <nathandanielcoleman@gmail.com>
2017-01-17 22:10:22 -0600

    Create new dir structure for league templates

--- musicleague/routes/league.py
+++ musicleague/routes/league.py
@@ -124,7 +124,7 @@ def save_league_settings(league_id, **kwargs):
 
 
 @app.route(VIEW_LEAGUE_URL, methods=['GET'])
-@templated('league/page.html')
+@templated('league/view/page.html')
 @login_required
 @league_required
 def view_league(league_id, **kwargs):
