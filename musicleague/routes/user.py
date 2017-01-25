1694eb5beef10e6e509069a6b625905520ce588a
Nathan Coleman <nathandanielcoleman@gmail.com>
2017-01-24 21:51:26 -0600

    Exclude creating user in autocomplete results

--- musicleague/routes/user.py
+++ musicleague/routes/user.py
@@ -30,7 +30,7 @@ def autocomplete():
     term = request.form.get('query')
     results = User.objects(name__icontains=term).all().limit(10)
     results = [{'label': user.name, 'id': user.id}
-               for user in results]
+               for user in results if user.id != g.user.id]
     return json.dumps(sorted(results, key=lambda u: u['label']))
 
 
