c9c902af3c901d613edec8eb3fcfade6e7504871
Nathan Coleman <nathandanielcoleman@gmail.com>
2017-02-17 16:35:08 -0600

    Wire up/down voting controls to status bar count

--- musicleague/static/js/vote.js
+++ musicleague/static/js/vote.js
@@ -1,25 +1,53 @@
-$('.up-btn').on("click", function() {
-    var wrapper = $(this).parent();
+function pad(n) {
+    return (n < 10) ? ("0" + n) : n;
+}
+
+function setSongStateUpvote(song) {
+    var wrapper = song.find('.up-down-btn-wrapper');
     var voteCount = wrapper.find('.vote-count');
-
-    wrapper.addClass("voted");
     var votes = Number(voteCount.html()) + 1;
-    voteCount.html(votes);
+    var currentVotes = $('#num-spent');
+    var numMaxVotes = Number($('#max-votes').html());
 
-    if (votes > 0)
+    if (votes <= numMaxVotes) {
         wrapper.addClass("voted");
-    else
+        voteCount.html(votes);
+        var newCurrentVotes = Number(currentVotes.html()) + 1;
+        currentVotes.html(pad(newCurrentVotes));
+    } else if (votes > numMaxVotes) {
+        wrapper.addClass("voted");
+    } else {
         wrapper.removeClass("voted");
+    }
+}
+
+function setSongStateDownvote(song) {
+    var wrapper = song.find('.up-down-btn-wrapper');
+    var voteCount = wrapper.find('.vote-count');
+    var votes = Number(voteCount.html()) - 1;
+    var currentVotes = $('#num-spent');
+
+    if (votes > 0) {
+        wrapper.addClass("voted");
+        voteCount.html(votes);
+        var newCurrentVotes = Number(currentVotes.html()) + 1;
+        currentVotes.html(pad(newCurrentVotes));
+    } else if (votes == 0) {
+        wrapper.removeClass("voted");
+        voteCount.html(votes);
+        var newCurrentVotes = Number(currentVotes.html()) + 1;
+        currentVotes.html(pad(newCurrentVotes));
+    } else {
+        wrapper.removeClass("voted");
+    }
+}
+
+$('.up-btn').on("click", function() {
+    var song = $(this).parent().parent();
+    setSongStateUpvote(song);
 });
 
 $('.down-btn').on("click", function() {
-    var wrapper = $(this).parent();
-    var voteCount = wrapper.find('.vote-count');
-    var votes = Number(voteCount.html()) - 1;
-    voteCount.html(votes);
-
-    if (votes > 0)
-        wrapper.addClass("voted");
-    else
-        wrapper.removeClass("voted");
+    var song = $(this).parent().parent();
+    setSongStateDownvote(song);
 });
