b375be475a05de8db9962f35488519f3504ba44a
Nathan Coleman <nathandanielcoleman@gmail.com>
2017-11-05 22:48:41 -0500

    Update league, round tables to use varchar id

--- musicleague/persistence/statements.py
+++ musicleague/persistence/statements.py
@@ -1,24 +1,46 @@
+# =====
+# USERS
+# =====
+
 CREATE_TABLE_USERS = """CREATE TABLE IF NOT EXISTS users (
                             id VARCHAR(255) NOT NULL PRIMARY KEY,
                             email VARCHAR(255) NOT NULL,
                             joined TIMESTAMP DEFAULT NOW(),
                             name VARCHAR(255) DEFAULT '');"""
 
+INSERT_USER = "INSERT INTO users (id, email, name) VALUES (%s, %s, %s) ON CONFLICT (id) DO NOTHING;"
+
+UPDATE_USER = "UPDATE users SET (email, name) = (%s, %s) WHERE id = %s;"
+
+# =======
+# LEAGUES
+# =======
+
 CREATE_TABLE_LEAGUES = """CREATE TABLE IF NOT EXISTS leagues (
-                            id SERIAL PRIMARY KEY,
+                            id VARCHAR(255) PRIMARY KEY,
                             created TIMESTAMP DEFAULT NOW(),
                             name VARCHAR(255) NOT NULL);"""
 
+INSERT_LEAGUE = "INSERT INTO leagues (id, name) VALUES (%s, %s) ON CONFLICT (id) DO NOTHING;"
+
+UPDATE_LEAGUE = "UPDATE leagues SET (name) = (%s) WHERE id = %s;"
+
+# ======
+# ROUNDS
+# ======
+
 CREATE_TABLE_ROUNDS = """CREATE TABLE IF NOT EXISTS rounds (
-                            id SERIAL PRIMARY KEY,
+                            id VARCHAR(255) PRIMARY KEY,
                             created TIMESTAMP DEFAULT NOW(),
                             description VARCHAR(255) DEFAULT '',
-                            league_id SERIAL REFERENCES leagues(id),
+                            league_id VARCHAR(255) REFERENCES leagues(id),
                             name VARCHAR(255) NOT NULL,
                             playlist_url VARCHAR(255) DEFAULT '',
                             submissions_due TIMESTAMP NOT NULL,
                             votes_due TIMESTAMP NOT NULL);"""
 
-INSERT_USER = "INSERT INTO users (id, email, name) VALUES (%s, %s, %s) ON CONFLICT (id) DO NOTHING;"
+INSERT_ROUND = """INSERT INTO rounds (id, description, league_id, name, submissions_due, votes_due)
+                    VALUES (%s, %s, %s, %s, %s, %s) ON CONFLICT (id) DO NOTHING;"""
 
-UPDATE_USER = "UPDATE users SET (email, name) = (%s, %s) WHERE id = %s;"
+UPDATE_ROUND = """UPDATE rounds SET (description, name, submissions_due, votes_due)
+                    VALUES (%s, %s, %s, %s) WHERE id = %s;"""
