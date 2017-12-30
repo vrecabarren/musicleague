092dff59bf75867ad4448542f236395465b64f29
Nathan Coleman <nathandanielcoleman@gmail.com>
2017-12-29 22:15:18 -0600

    Correct typo in log message for voting endpoint

--- musicleague/routes/vote.py
+++ musicleague/routes/vote.py
@@ -67,61 +67,64 @@ def view_vote(league_id, submission_period_id):
 @login_required
 def vote(league_id, submission_period_id):
     start = timer()
+
     league = select_league(league_id)
+    app.logger.info('League loaded after %ss', timer() - start)
+
     submission_period = next((sp for sp in league.submission_periods
                               if sp.id == submission_period_id), None)
-    app.logger.info('Submission period loaded after %s s', timer() - start)
+
     if not league or not submission_period:
         return "No submission period or league", httplib.INTERNAL_SERVER_ERROR
 
     if not league.has_user(g.user):
-        app.logger.warning('No membership - redirecting after %s s', timer() - start)
+        app.logger.warning('No membership - redirecting after %ss', timer() - start)
         return "Not a member of this league", httplib.UNAUTHORIZED
 
     # If this user didn't submit for this round, don't allow them to vote
     if g.user not in submission_period.have_submitted:
-        app.logger.warning('No submission - redirecting after %s s', timer() - start)
+        app.logger.warning('No submission - redirecting after %ss', timer() - start)
         return redirect(url_for('view_league', league_id=league_id))
 
     # If this round is no longer accepting votes, redirect
     if not submission_period.accepting_votes:
-        app.logger.warning('No longer accepting votes - redirecting after %s s', timer() - start)
+        app.logger.warning('No longer accepting votes - redirecting after %ss', timer() - start)
         return redirect(request.referrer)
 
     try:
-        app.logger.info('Loading votes from form submission after %s s', timer() - start)
+        app.logger.info('Loading votes from form submission after %ss', timer() - start)
         votes = json.loads(request.form.get('votes'))
-        app.logger.info('Votes loaded after %s s', timer() - start)
+        app.logger.info('Votes loaded after %ss', timer() - start)
     except Exception:
         app.logger.exception("Failed to load JSON from form with votes: %s",
                              request.form)
         return 'There was an error processing your votes', 500
 
     # Remove all unnecessary zero-values
-    app.logger.info('Removing zero values after %s s', timer() - start)
+    app.logger.info('Removing zero values after %ss', timer() - start)
     votes = {k: v for k, v in votes.iteritems() if v}
-    app.logger.info('Removed zero values after %s s', timer() - start)
+    app.logger.info('Removed zero values after %ss', timer() - start)
 
     # Process votes
-    app.logger.info('Creating/updating vote after %s s', timer() - start)
+    app.logger.info('Creating/updating vote after %ss', timer() - start)
     vote = create_or_update_vote(votes, submission_period, league, g.user)
-    app.logger.info('Vote created/updated after %s s', timer() - start)
+    app.logger.info('Vote created/updated after %ss', timer() - start)
 
     # If someone besides owner is voting, notify the owner
     if g.user.id != league.owner.id:
-        app.logger.info('Notifying owner of user vote after %s s', timer() - start)
+        app.logger.info('Notifying owner of user vote after %ss', timer() - start)
         owner_user_voted_notification(vote)
 
-    app.logger.info('Checking remaining voters after %s s', timer() - start)
+    app.logger.info('Checking remaining voters after %ss', timer() - start)
     remaining = submission_period.have_not_voted
     if not remaining:
-        app.logger.info('Completing submission period after %s s', timer() - start)
+        app.logger.info('Completing submission period after %ss', timer() - start)
         complete_submission_period.delay(submission_period.id)
 
     elif vote.count < 2 and len(remaining) == 1:
-        app.logger.info('Notifying final voter after %s s', timer() - start)
+        app.logger.info('Notifying final voter after %ss', timer() - start)
         last_user = remaining[0]
         user_last_to_vote_notification(last_user, submission_period)
 
-    app.logger.info('Vote request complete after %s s', timer() - start)
+    app.logger.info('Vote request complete after %ss', timer() - start)
     return redirect(url_for('view_league', league_id=league_id))
