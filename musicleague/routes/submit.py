12aaaeb6767798c1b7b92856ecac8d560c6ec7ba
Nathan Coleman <nathandanielcoleman@gmail.com>
2018-01-20 00:05:53 -0500

    Add contextual logs for failure to process submits/votes

--- musicleague/routes/submit.py
+++ musicleague/routes/submit.py
@@ -54,79 +54,85 @@ def view_submit(league_id, submission_period_id):
 @app.route(SUBMIT_URL, methods=['POST'])
 @login_required
 def submit(league_id, submission_period_id):
-    # TODO: Way too much happens in this function
-    league = select_league(league_id)
-    submission_period = next((sp for sp in league.submission_periods
-                              if sp.id == submission_period_id), None)
-    if not league or not submission_period:
-        return "No submission period or league", httplib.INTERNAL_SERVER_ERROR
-
-    if not league.has_user(g.user):
-        return "Not a member of this league", httplib.UNAUTHORIZED
-
-    if (not submission_period.accepting_submissions and
-            not submission_period.accepting_late_submissions):
-        return redirect(request.referrer)
-
     try:
-        tracks = json.loads(request.form.get('songs'))
+        # TODO: Way too much happens in this function
+        league = select_league(league_id)
+        submission_period = next((sp for sp in league.submission_periods
+                                  if sp.id == submission_period_id), None)
+        if not league or not submission_period:
+            return "No submission period or league", httplib.INTERNAL_SERVER_ERROR
+
+        if not league.has_user(g.user):
+            return "Not a member of this league", httplib.UNAUTHORIZED
+
+        if (not submission_period.accepting_submissions and
+                not submission_period.accepting_late_submissions):
+            return redirect(request.referrer)
+
+        try:
+            tracks = json.loads(request.form.get('songs'))
+        except Exception:
+            app.logger.exception("Failed to load JSON from form with submit: %s",
+                                 request.form)
+            return 'There was an error processing your submission', 500
+
+        if None in tracks:
+            return redirect(request.referrer)
+
+        # Don't allow user to submit duplicate tracks
+        if len(tracks) != len(set(tracks)):
+            return redirect(request.referrer)
+
+        # Don't include user's own previous submission when checking duplicates
+        my_submission = get_my_submission(g.user, submission_period)
+        their_tracks = []
+        if submission_period.all_tracks:
+            their_tracks = set(submission_period.all_tracks)
+            if my_submission is not None:
+                their_tracks.difference_update(set(my_submission.tracks))
+            their_tracks = list(their_tracks)
+
+        if their_tracks:
+            s_tracks = tracks + their_tracks
+            s_tracks = g.spotify.tracks(s_tracks).get('tracks')
+            my_tracks = s_tracks[:len(tracks)]
+            their_tracks = s_tracks[len(tracks):]
+
+            # Don't allow user to submit already submitted track, album or artist
+            duplicate_tracks = check_duplicate_tracks(my_tracks, their_tracks)
+            duplicate_albums = check_duplicate_albums(my_tracks, their_tracks)
+            duplicate_artists = check_duplicate_artists(my_tracks, their_tracks)
+            if duplicate_tracks or duplicate_albums or duplicate_artists:
+                return render_template(
+                    'submit/page.html',
+                    user=g.user, league=league, round=submission_period,
+                    previous_tracks=tracks,
+                    duplicate_songs=duplicate_tracks,
+                    duplicate_albums=duplicate_albums,
+                    duplicate_artists=duplicate_artists,
+                    access_token=session['access_token'])
+
+        # Create a new submission on the round as current user
+        submission = create_or_update_submission(
+            tracks, submission_period, league, g.user)
+
+        # If someone besides owner is submitting, notify the owner
+        if g.user.id != league.owner.id:
+            owner_user_submitted_notification(submission)
+
+        remaining = submission_period.have_not_submitted
+        if not remaining:
+            complete_submission_process.delay(submission_period.id)
+
+        # Don't send submission reminder if this user is resubmitting. In this
+        # case, the last user to submit will have already gotten a notification.
+        elif submission.count < 2 and len(remaining) == 1:
+            last_user = remaining[0]
+            user_last_to_submit_notification(last_user, submission_period)
+
+        return redirect(url_for('view_league', league_id=league_id))
+
     except Exception:
-        app.logger.exception("Failed to load JSON from form with submit: %s",
-                             request.form)
-        return 'There was an error processing your submission', 500
-
-    if None in tracks:
-        return redirect(request.referrer)
-
-    # Don't allow user to submit duplicate tracks
-    if len(tracks) != len(set(tracks)):
-        return redirect(request.referrer)
-
-    # Don't include user's own previous submission when checking duplicates
-    my_submission = get_my_submission(g.user, submission_period)
-    their_tracks = []
-    if submission_period.all_tracks:
-        their_tracks = set(submission_period.all_tracks)
-        if my_submission is not None:
-            their_tracks.difference_update(set(my_submission.tracks))
-        their_tracks = list(their_tracks)
-
-    if their_tracks:
-        s_tracks = tracks + their_tracks
-        s_tracks = g.spotify.tracks(s_tracks).get('tracks')
-        my_tracks = s_tracks[:len(tracks)]
-        their_tracks = s_tracks[len(tracks):]
-
-        # Don't allow user to submit already submitted track, album or artist
-        duplicate_tracks = check_duplicate_tracks(my_tracks, their_tracks)
-        duplicate_albums = check_duplicate_albums(my_tracks, their_tracks)
-        duplicate_artists = check_duplicate_artists(my_tracks, their_tracks)
-        if duplicate_tracks or duplicate_albums or duplicate_artists:
-            return render_template(
-                'submit/page.html',
-                user=g.user, league=league, round=submission_period,
-                previous_tracks=tracks,
-                duplicate_songs=duplicate_tracks,
-                duplicate_albums=duplicate_albums,
-                duplicate_artists=duplicate_artists,
-                access_token=session['access_token'])
-
-    # Create a new submission on the round as current user
-    submission = create_or_update_submission(
-        tracks, submission_period, league, g.user)
-
-    # If someone besides owner is submitting, notify the owner
-    if g.user.id != league.owner.id:
-        owner_user_submitted_notification(submission)
-
-    remaining = submission_period.have_not_submitted
-    if not remaining:
-        complete_submission_process.delay(submission_period.id)
-
-    # Don't send submission reminder if this user is resubmitting. In this
-    # case, the last user to submit will have already gotten a notification.
-    elif submission.count < 2 and len(remaining) == 1:
-        last_user = remaining[0]
-        user_last_to_submit_notification(last_user, submission_period)
-
-    return redirect(url_for('view_league', league_id=league_id))
+        app.logger.exception(
+            'Failed to process submissions',
+            extra={'user': g.user.id, 'league': league_id, 'round': submission_period_id})
--- musicleague/routes/vote.py
+++ musicleague/routes/vote.py
@@ -66,66 +66,72 @@ def view_vote(league_id, submission_period_id):
 @app.route(VOTE_URL, methods=['POST'])
 @login_required
 def vote(league_id, submission_period_id):
-    start = timer()
+    try:
+        start = timer()
 
-    league = select_league(league_id)
-    app.logger.info('League for %s loaded after %ss', submission_period_id, timer() - start)
+        league = select_league(league_id)
+        app.logger.info('League for %s loaded after %ss', submission_period_id, timer() - start)
 
-    submission_period = next((sp for sp in league.submission_periods
-                              if sp.id == submission_period_id), None)
+        submission_period = next((sp for sp in league.submission_periods
+                                  if sp.id == submission_period_id), None)
 
-    if not league or not submission_period:
-        return "No submission period or league", httplib.INTERNAL_SERVER_ERROR
+        if not league or not submission_period:
+            return "No submission period or league", httplib.INTERNAL_SERVER_ERROR
 
-    if not league.has_user(g.user):
-        app.logger.warning('No membership - redirecting after %ss', timer() - start)
-        return "Not a member of this league", httplib.UNAUTHORIZED
+        if not league.has_user(g.user):
+            app.logger.warning('No membership - redirecting after %ss', timer() - start)
+            return "Not a member of this league", httplib.UNAUTHORIZED
 
-    # If this user didn't submit for this round, don't allow them to vote
-    if not get_my_submission(g.user, submission_period):
-        app.logger.warning('No submission - redirecting after %ss', timer() - start)
+        # If this user didn't submit for this round, don't allow them to vote
+        if not get_my_submission(g.user, submission_period):
+            app.logger.warning('No submission - redirecting after %ss', timer() - start)
+            return redirect(url_for('view_league', league_id=league_id))
+
+        # If this round is no longer accepting votes, redirect
+        if not submission_period.accepting_votes:
+            app.logger.warning('Round %s no longer accepting votes - redirecting after %ss',
+                               submission_period_id, timer() - start)
+            return redirect(request.referrer)
+
+        try:
+            app.logger.info('Loading votes for %s after %ss', submission_period_id, timer() - start)
+            votes = json.loads(request.form.get('votes'))
+            app.logger.info('Votes for %s loaded after %ss', submission_period_id, timer() - start)
+        except Exception:
+            app.logger.exception("Failed to load JSON from form with votes: %s",
+                                 request.form)
+            return 'There was an error processing votes', 500
+
+        # Remove all unnecessary zero-values
+        app.logger.info('Removing zero values for %s after %ss', submission_period_id, timer() - start)
+        votes = {k: v for k, v in votes.iteritems() if v}
+        app.logger.info('Removed zero values for %s after %ss', submission_period_id, timer() - start)
+
+        # Process votes
+        app.logger.info('Creating/updating vote for %s after %ss', submission_period_id, timer() - start)
+        vote = create_or_update_vote(votes, submission_period, league, g.user)
+        app.logger.info('Vote created/updated for %s after %ss', submission_period_id, timer() - start)
+
+        # If someone besides owner is voting, notify the owner
+        if g.user.id != league.owner.id:
+            app.logger.info('Notifying owner of vote for %s after %ss', submission_period_id, timer() - start)
+            owner_user_voted_notification(vote)
+
+        app.logger.info('Checking remaining voters for %s after %ss', submission_period_id, timer() - start)
+        remaining = submission_period.have_not_voted
+        if not remaining:
+            app.logger.info('Completing submission period %s after %ss', submission_period_id, timer() - start)
+            complete_submission_period.delay(submission_period.id)
+
+        elif vote.count < 2 and len(remaining) == 1:
+            app.logger.info('Notifying final voter for %s after %ss', submission_period_id, timer() - start)
+            last_user = remaining[0]
+            user_last_to_vote_notification(last_user, submission_period)
+
+        app.logger.info('Vote request for %s complete after %ss', submission_period_id, timer() - start)
         return redirect(url_for('view_league', league_id=league_id))
 
-    # If this round is no longer accepting votes, redirect
-    if not submission_period.accepting_votes:
-        app.logger.warning('Round %s no longer accepting votes - redirecting after %ss',
-                           submission_period_id, timer() - start)
-        return redirect(request.referrer)
-
-    try:
-        app.logger.info('Loading votes for %s after %ss', submission_period_id, timer() - start)
-        votes = json.loads(request.form.get('votes'))
-        app.logger.info('Votes for %s loaded after %ss', submission_period_id, timer() - start)
     except Exception:
-        app.logger.exception("Failed to load JSON from form with votes: %s",
-                             request.form)
-        return 'There was an error processing votes', 500
-
-    # Remove all unnecessary zero-values
-    app.logger.info('Removing zero values for %s after %ss', submission_period_id, timer() - start)
-    votes = {k: v for k, v in votes.iteritems() if v}
-    app.logger.info('Removed zero values for %s after %ss', submission_period_id, timer() - start)
-
-    # Process votes
-    app.logger.info('Creating/updating vote for %s after %ss', submission_period_id, timer() - start)
-    vote = create_or_update_vote(votes, submission_period, league, g.user)
-    app.logger.info('Vote created/updated for %s after %ss', submission_period_id, timer() - start)
-
-    # If someone besides owner is voting, notify the owner
-    if g.user.id != league.owner.id:
-        app.logger.info('Notifying owner of vote for %s after %ss', submission_period_id, timer() - start)
-        owner_user_voted_notification(vote)
-
-    app.logger.info('Checking remaining voters for %s after %ss', submission_period_id, timer() - start)
-    remaining = submission_period.have_not_voted
-    if not remaining:
-        app.logger.info('Completing submission period %s after %ss', submission_period_id, timer() - start)
-        complete_submission_period.delay(submission_period.id)
-
-    elif vote.count < 2 and len(remaining) == 1:
-        app.logger.info('Notifying final voter for %s after %ss', submission_period_id, timer() - start)
-        last_user = remaining[0]
-        user_last_to_vote_notification(last_user, submission_period)
-
-    app.logger.info('Vote request for %s complete after %ss', submission_period_id, timer() - start)
-    return redirect(url_for('view_league', league_id=league_id))
+        app.logger.exception(
+            'Failed to process votes',
+            extra={'user': g.user.id, 'league': league.id, 'round': submission_period_id})
