7cd1df7a3ac02bbd238511d0fd16df9bd9909767
Nathan Coleman <nathandanielcoleman@gmail.com>
2018-10-28 17:19:52 -0400

    Clean up bcc logic

--- musicleague/notify/email.py
+++ musicleague/notify/email.py
@@ -64,20 +64,18 @@ def user_all_voted_email(submission_period):
         return
 
     to = submission_period.league.owner.email
-    additional_data = {}
 
-    bcc_list = ','.join(
-        [u.email for u in submission_period.league.users
-         if u.email and not submission_period.league.has_owner(u)])
-    if bcc_list:
-        additional_data['bcc'] = bcc_list
+    bcc_list = [
+        u.email for u in submission_period.league.users
+        if u.email and not submission_period.league.has_owner(u)
+    ]
 
     _send_email.delay(
         to,
         'Music League - The Votes Are In',
         _txt_email('all_voted.txt', submission_period=submission_period),
         _html_email('all_voted.html', submission_period=submission_period),
-        additional_data=additional_data
+        bcc_list=(bcc_list or None)
     )
 
 
@@ -127,21 +125,19 @@ def user_new_round_email(submission_period):
         return
 
     to = submission_period.league.owner.email
-    additional_data = {}
 
-    bcc_list = ','.join(
-        [u.email for u in submission_period.league.users
-         if u.email and not submission_period.league.has_owner(u)
-         and u.preferences.user_playlist_created_notifications])
-    if bcc_list:
-        additional_data['bcc'] = bcc_list
+    bcc_list = [
+        u.email for u in submission_period.league.users
+        if u.email and not submission_period.league.has_owner(u)
+        and u.preferences.user_playlist_created_notifications
+    ]
 
     _send_email.delay(
         to,
         'Music League - Round Starting',
         _txt_email('new_round.txt', submission_period=submission_period),
         _html_email('new_round.html', submission_period=submission_period),
-        additional_data=additional_data
+        bcc_list=(bcc_list or None)
     )
 
 
@@ -155,21 +151,19 @@ def user_playlist_created_email(submission_period):
         return
 
     to = submission_period.league.owner.email
-    additional_data = {}
 
-    bcc_list = ','.join(
-        [u.email for u in submission_period.league.users
-         if u.email and not submission_period.league.has_owner(u)
-         and u.preferences.user_playlist_created_notifications])
-    if bcc_list:
-        additional_data['bcc'] = bcc_list
+    bcc_list = [
+        u.email for u in submission_period.league.users
+        if u.email and not submission_period.league.has_owner(u)
+        and u.preferences.user_playlist_created_notifications
+    ]
 
     _send_email.delay(
         to,
         'Music League - New Playlist',
         _txt_email('playlist.txt', submission_period=submission_period),
         _html_email('playlist.html', submission_period=submission_period),
-        additional_data=additional_data
+        bcc_list=(bcc_list or None)
     )
 
 
@@ -205,7 +199,7 @@ def user_vote_reminder_email(user, submission_period):
 
 
 @job('default', connection=redis_conn)
-def _send_email(to, subject, text, html, additional_data=None):
+def _send_email(to, subject, text, html, bcc_list=None):
     if not is_deployed():
         app.logger.info(text)
         return
@@ -218,15 +212,11 @@ def _send_email(to, subject, text, html, additional_data=None):
     mail.add_content(Content("text/plain", text))
     mail.add_content(Content("text/html", html))
 
-    if additional_data is None:
-        additional_data = {}
-
-    if 'bcc' in additional_data:
-        bcc_list = additional_data.get('bcc', '')
-        for bcc in bcc_list.split(','):
-            bcc_personalization = Personalization()
+    if bcc_list is not None:
+        bcc_personalization = Personalization()
+        for bcc in bcc_list:
             bcc_personalization.add_bcc(Email(bcc))
-            mail.add_personalization(Personalization())
+        mail.add_personalization(bcc_personalization)
 
     sg = sendgrid.SendGridAPIClient(apikey=api_key)
     response = sg.client.mail.send.post(request_body=mail.get())
