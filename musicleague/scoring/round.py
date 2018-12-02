da2ff40335d7a49435a921864c78bcb9e2db9e6d
Nathan Coleman <nathandanielcoleman@gmail.com>
2018-12-02 13:33:48 -0500

    Remove points filter when calculating round scoreboard

--- musicleague/scoring/round.py
+++ musicleague/scoring/round.py
@@ -32,7 +32,7 @@ def calculate_round_scoreboard(round):
     # Get Votes for each entry
     for vote in round.votes:
         for uri, points in vote.votes.iteritems():
-            if points != 0 and uri in entries:
+            if uri in entries:
                 entries[uri].votes.append(vote)
 
     # Sort votes on each entry by number of points awarded
