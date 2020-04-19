3231e599ce9e847a5cec0a8afae8be4aae7f2e7b
Nathan Coleman <nathandanielcoleman@gmail.com>
2020-04-19 11:55:10 -0400

    Fix post request kwargs

--- musicleague/routes/league.py
+++ musicleague/routes/league.py
@@ -87,8 +87,8 @@ def post_create_league_v2():
         r = requests.post('https://musicleague-server.herokuapp.com/v1/leagues/' + league_id + '/rounds',
             data=json.dumps(
                 {'name': new_round['name'], 'description': new_round['description'],
-                 'submissionsDue': submission_due_date, 'votesDue': vote_due_date},
-            headers=auth_headers))
+                 'submissionsDue': submission_due_date, 'votesDue': vote_due_date}),
+            headers=auth_headers)
 
         app.logger.info('Successful post to API server', extra={'resp': r.json()})
 
